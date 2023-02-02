e60427fcc5f9c57c45117a81f6bfb6f77b272d6a
jyc <80881099@qq.vom>
2023-02-02 15:24:55 +0800

    Delete static files

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/1', function(req, res, next) {
+  res.json({"t":1});
 });
 
 module.exports = router;
