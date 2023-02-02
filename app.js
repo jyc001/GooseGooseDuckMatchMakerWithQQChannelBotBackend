ad7229691fd6613f35628ea4954a42b383a5957f
jyc001 <80881099@qq.com>
2023-02-02 16:09:09 +0800

    format app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
